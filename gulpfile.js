c8834f73e60a81999500f97bea4675420e185b94
Hossein Karami <hi@hosseinkarami.com>
2018-04-05 08:49:50 +0430

    hello

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    header  = require('gulp-header'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    cssnano = require('gulp-cssnano'),
+    sourcemaps = require('gulp-sourcemaps'),
+    package = require('./package.json');
+
+var paths = {
+    scripts: ['node_modules/jquery/dist/jquery.min.js', 'src/js/scripts.js']
+};
+var banner = [
+  '/*!\n' +
+  ' +----------------------------------------------------+\n' +
+  ' * <%= package.title %>                         *\n' +
+  ' * @authors <%= package.author %> *\n' +
+  ' * <%= package.url %>                                  *\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.                      *\n' +
+  ' +----------------------------------------------------+\n' +
+  ' */',
+  '\n'
+].join('');
+
+gulp.task('css', function () {
+    return gulp.src('src/scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(header(banner, { package : package }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('js',function(){
+  gulp.src(paths.scripts)
+    .pipe(sourcemaps.init())
+    .pipe(header(banner, { package : package }))
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(uglify())
+    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+    .pipe(header(banner, { package : package }))
+    .pipe(concat('scripts.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(browserSync.reload({stream:true, once: true}));
+});
+
+gulp.task('browser-sync', ['css', 'js'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+});
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['css', 'js', 'browser-sync'], function () {
+    gulp.watch("src/scss/**/*.scss", ['css']);
+    gulp.watch("src/js/*.js", ['js']);
+    gulp.watch("app/*.html", ['bs-reload']);
+});
