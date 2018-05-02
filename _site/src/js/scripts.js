d77bef8b886bc5bcbac4f3f5ff5c9200dfe042f9
Hossein Karami <hi@hosseinkarami.com>
2018-05-02 14:34:39 +0430

    2018

--- _site/src/js/scripts.js
+++ _site/src/js/scripts.js
@@ -8,3 +8,86 @@ $(window).scroll(function () {
     $('.head-titles').removeClass('fixed-content');
   }
 });
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 150) {
+    $('.viewbys').addClass('fixedView');
+  }
+  else {
+    $('.viewbys').removeClass('fixedView');
+  }
+});
+// $('.gotoGroup li a')
+//   // Remove links that don't actually link to anything
+//   .not('[href="#"]')
+//   .not('[href="#0"]')
+//   .click(function (event) {
+//     // On-page links
+//     if (
+//       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+//       &&
+//       location.hostname == this.hostname
+//     ) {
+//       // Figure out element to scroll to
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//       // Does a scroll target exist?
+//       if (target.length) {
+//         // Only prevent default if animation is actually gonna happen
+//         event.preventDefault();
+//         $('html, body').animate({
+//           scrollTop: (target.offset().top - 54)
+//         }, 1000, function () {
+//           // Callback after animation
+//           // Must change focus!
+//           var $target = $(target);
+//           $target.focus();
+//           if ($target.is(":focus")) { // Checking if the target was focused
+//             return false;
+//           } else {
+//             $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+//             $target.focus(); // Set focus again
+//           };
+//         });
+//       }
+//     }
+//   });
+
+// $(function () {
+//   var $grouplink = $('.gotoGroup li a').click(function () {
+//     $grouplink.removeClass('current');
+//     $(this).addClass('current');
+//   });
+// });
+
+$('.gotoGroup li a').on('click', function () {
+
+  var scrollAnchor = $(this).attr('data-scroll'),
+    scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top + 0;
+
+  $('body,html').animate({
+    scrollTop: scrollPoint
+  }, 500);
+
+  return false;
+
+})
+
+
+$(window).scroll(function () {
+  var windscroll = $(window).scrollTop();
+  if (windscroll >= 100) {
+
+    $('.all-matches .groups').each(function (i) {
+      if ($(this).position().top <= windscroll + 0) {
+        $('.gotoGroup li a.current').removeClass('current');
+        $('.gotoGroup li a').eq(i).addClass('current');
+      }
+    });
+
+  } else {
+
+    $('.gotoGroup li a.current').removeClass('current');
+    $('.gotoGroup li a:first').addClass('current');
+  }
+
+}).scroll();
