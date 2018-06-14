a497cb22e8586af334b7c350fd12c8e0d818db3b
Hossein Karami <hi@hosseinkarami.com>
2018-06-15 02:05:42 +0430

    add roundTime feature

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1178,3 +1178,64 @@ $('.gotoGroup li a')
 
 
 $('body').scrollspy({ target: '#group-name', offset: 170 });
+
+
+
+// Date.parse('14 Jun 2018 18:00:00 GMT')
+
+
+
+
+function timerGo(ele) {
+  var round1start = new Date($(ele).attr("data-round1start"));
+  var round1total = parseInt($(ele).attr("data-round1total"));
+
+  //var round2startText = $(ele).attr("data-round2start");
+  var round2start = new Date(round1start.getTime() + (round1total + 15) * 60 * 1000);
+  var round2total = parseInt($(ele).attr("data-round2total"));
+
+  var now = new Date();
+  var round1end = new Date(round1start.getTime() + round1total * 60 * 1000);
+  var round2end = new Date(round2start.getTime() + round2total * 60 * 1000);
+
+  if ((round1start < now && now < round1end) || (round2start < now && now < round2end)) {
+
+    leftMins = "";
+    if (now < round1end) {
+      leftMins = Math.round((now - round1start) / (1000 * 60));
+      if (leftMins > 45) {
+        leftMins = "45' (+" + (leftMins - 45) + ")";
+      } else {
+        leftMins = leftMins + "'";
+      }
+    } else {
+      leftMins = Math.floor((now - round2start) / (1000 * 60));
+      leftMins += 45;
+      if (leftMins > 90) {
+        leftMins = "90' (+" + (leftMins - 45) + ")";
+      } else {
+        leftMins = leftMins + "'";
+      }
+    }
+
+    $(ele).html(leftMins);
+  } else {
+    if (round1end < now && now < round2end) {
+      $(ele).html("پایان نیمه اول");
+    } else if (round2end < now) {
+      $(ele).html("نتیجه نهایی");
+    } else {
+      $(ele).html("");
+    }
+
+  }
+}
+
+function iterateTimers() {
+  $(".timer").each(function (i, ele) {
+    timerGo(ele);
+  });
+
+  setTimeout(iterateTimers, 1000);
+}
+iterateTimers();
